181bbb6943d12568de35f3a158507083ed98a5ed
bill1118 <bill1118921@gmail.com>
2024-09-18 00:30:53 +0800

    0918

---
+++ jsx/Topbar.jsx
@@ -0,0 +1,17 @@
+function Topbar(){
+    return(
+            <header>
+                <h1 className="logo">
+                    <a href=""><img src="" alt="" /></a>
+                </h1>
+                <nav className="nav">
+                    <ul className="menu">
+                        <li><a href="">酒類介紹</a></li>
+                        <li><a href="">品牌故事</a></li>
+                        <li><a href="">最新消息</a></li>
+                        <li><a href="">聯絡我們</a></li>
+                    </ul>
+                </nav>
+            </header>
+    );
+}
